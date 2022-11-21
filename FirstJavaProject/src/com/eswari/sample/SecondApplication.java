26f0d9444fee3c041ce4c66a332c703265480997
JagadeswariD <jagadeswaridhanagopal@gmail.com>
2022-11-21 16:41:38 -0500

    Second Commit

---
+++ FirstJavaProject/src/com/eswari/sample/SecondApplication.java
@@ -0,0 +1,10 @@
+package com.eswari.sample;
+
+public class SecondApplication {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Second Application");
+	}
+
+}
